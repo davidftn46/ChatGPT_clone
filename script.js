48b67d05986452c821870378dc0698bdae32851d
david <david@example.com>
2024-11-11 03:21:58 +0100

    delete btn and type-text animation

--- script.js
+++ script.js
@@ -2,23 +2,68 @@ const chatInput = document.querySelector("#chat-input");
 const sendButton = document.querySelector("#send-btn");
 const chatContainer = document.querySelector(".chat-container");
 const themeButton = document.querySelector("#theme-btn");
+const deleteButton = document.querySelector("#delete-btn");
 
 let userText = null;
+const API_KEY = "Stavite_vas_API_kljuc_ovde"; 
+/// Kljuc nisam ostavio iz bezbednosnih razloga, obzirom da je aplikacija samo klijentska,
+/// ne postoji bezbedan nacin da prikrijem kljuc u sustini, a ujedno i aplikaciju postavim na github
 
-const API_KEY = "";
+const initialHeight = chatInput.scrollHeight; 
+/// Uz pomoc ove promenljive, prilikom svakog refresh-a nase web stranice/aplikacije,
+/// nasa web stranica ce biti skrolovana na sam kraj chat-a
 
+/// Funkcija koja manipulise prikazom tekstualnog odgovora od strane chat-bota, karakter po karakter
+const typeText = (element, text, delay = 50, callback = () => {}) => {
+    let index = 0;
+    element.innerHTML = ""; 
+
+    const type = () => {
+        if (index < text.length) {
+            element.innerHTML += text.charAt(index);
+            index++;
+            setTimeout(type, delay);
+        } else {
+            callback();  
+        }
+    };
+    
+    type();
+};
+
+/// Funkcija koja ucitava sadrzaj (konverzaciju sa chat-botom, boju teme) iz lokalne memorije (local storage)
 const loadDataFromLocalstorage = () => {
     const themeColor = localStorage.getItem("theme-color");
 
     document.body.classList.toggle("light-mode", themeColor === "light_mode");
     themeButton.innerText = document.body.classList.contains("light-mode") ? "dark_mode" : "light_mode";
 
-    chatContainer.innerHTML = localStorage.getItem("all-chats");
+    const defaultText = `<div class="default-text">
+                            <h1 id="title"></h1>
+                            <p id="description-line1"></p>
+                            <p id="description-line2"></p>
+                         </div>`
+
+    chatContainer.innerHTML = localStorage.getItem("all-chats") || defaultText;
     chatContainer.scrollTo(0, chatContainer.scrollHeight);
+
+    if (!localStorage.getItem("all-chats")) {
+
+        typeText(document.getElementById("title"), "ChatGPT Clone", 60); 
+
+        setTimeout(() => typeText(document.getElementById("description-line1"), 
+        "Start a conversation and explore the power of AI.", 20), 900);
+    
+        setTimeout(() => typeText(document.getElementById("description-line2"), 
+        "Your chat history will be displayed here.", 20), 1800);
+
+    }
+
 }
 
 loadDataFromLocalstorage();
 
+/// Funkcija za kreiranje elemenata
 const createElement = (html, className) => {
     const chatDiv = document.createElement("div");
     chatDiv.classList.add("chat", className);
@@ -26,7 +71,8 @@ const createElement = (html, className) => {
     return chatDiv;
 }
 
-const getChatResponse = async (incomingChatDiv) =>{
+/// Asinhrona funkcija koja uz pomoc API-ja pribavlja odgovore od strane chat-bota, a ujedno i hendluje potencijalne greske
+const getChatResponse = async (incomingChatDiv) => {
     const API_URL = "https://api.cohere.ai/v1/generate";
     const pElement = document.createElement("p");
 
@@ -49,11 +95,21 @@ const getChatResponse = async (incomingChatDiv) =>{
     try {
 
         const response = await (await fetch(API_URL, requestOptions)).json();
-        pElement.textContent = response.generations[0].text.trim();
+        const fullText = response.generations[0].text.trim();
+
+        typeText(pElement, fullText, 10, () => {
+            localStorage.setItem("all-chats", chatContainer.innerHTML);
+        });
     
     } catch(error) {
 
-        console.log(error);
+        pElement.classList.add("error");
+        pElement.textContent = "Oops! Something went wrong while retrieving the response. Please try again.";
+
+        setTimeout(() => {
+            localStorage.setItem("all-chats", chatContainer.innerHTML);
+        }, 1000);
+
     }
 
     incomingChatDiv.querySelector(".typing-animation").remove();
@@ -62,6 +118,7 @@ const getChatResponse = async (incomingChatDiv) =>{
     localStorage.setItem("all-chats", chatContainer.innerHTML);
 }
 
+/// Funkcija koja sluzi za manipulisanje copy dugmeta
 const copyResponse = (copyBtn) => {
     const responseTextElement = copyBtn.parentElement.querySelector("p");
     navigator.clipboard.writeText(responseTextElement.textContent);
@@ -69,6 +126,7 @@ const copyResponse = (copyBtn) => {
     setTimeout(() => copyBtn.textContent = "content_copy", 1000);
 }
 
+/// Funkcija koja simulira cekanje odgovora od strane chat-bota (animacija sa 3 tacke)
 const showTypingAnimation = () => {
     const html = `<div class="chat-content">
                     <div class="chat-details">
@@ -89,9 +147,13 @@ const showTypingAnimation = () => {
 
 }
 
+/// Funkcija koja pribavlja pitanja od klijenta (korisnika aplikacije)
 const handleOutgoingChat = () => {
     userText = chatInput.value.trim();
     if(!userText) return;
+
+    chatInput.value = "";
+    chatInput.style.height = `${initialHeight}px`;
 
     const html = `<div class="chat-content">
                     <div class="chat-details">
@@ -102,15 +164,40 @@ const handleOutgoingChat = () => {
     
     const outgoingChatDiv = createElement(html, "outgoing");
     outgoingChatDiv.querySelector("p").textContent = userText;
+    document.querySelector(".default-text")?.remove();
     chatContainer.appendChild(outgoingChatDiv);
     chatContainer.scrollTo(0, chatContainer.scrollHeight);
     setTimeout(showTypingAnimation, 500);
 }
 
+/// Listener koji sluzi za menjanje tema (dark/light mode) od strane korisnika
 themeButton.addEventListener("click", () => {
     document.body.classList.toggle("light-mode");
     localStorage.setItem("theme-color", themeButton.innerText);
     themeButton.innerText = document.body.classList.contains("light-mode") ? "dark_mode" : "light_mode";
 });
 
+/// Listener koji sluzi za brisanje sadrzaja chat-a
+deleteButton.addEventListener("click", () => {
+    if(confirm("Are you sure you want to delete all the chats?")) {
+        localStorage.removeItem("all-chats");
+        loadDataFromLocalstorage();
+    }
+});
+
+/// Listener koji sluzi za manipulaciju polja za unos teksta (podesava njegovu "visinu")
+chatInput.addEventListener("input", () => {
+    chatInput.style.height = `${initialHeight}px`
+    chatInput.style.height = `${chatInput.scrollHeight}px`
+});
+
+/// Listener koji sluzi za manipulaciju "Enter" dugmeta na tastaturi 
+chatInput.addEventListener("keydown", (e) => {
+    if(e.key === "Enter" && !e.shiftKey && window.innerWidth > 800) {
+        e.preventDefault();
+        handleOutgoingChat();
+    }
+}); 
+
+/// Listener koji sluzi za manipulaciju send button-a
 sendButton.addEventListener("click", handleOutgoingChat);
